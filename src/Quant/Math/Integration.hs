20d9dd1d3a2905a2b465ee44a2186f639cfe8819
Timothy Dees <timothy.dees@gmail.com>
2015-03-20 14:47:33 -0700

    Initial commit
    Currently just midpoint/trapezoid/simpson

---
+++ src/Quant/Math/Integration.hs
@@ -0,0 +1,24 @@
+module Quant.Math.Integration (
+    Integrator
+  , midpoint
+  , trapezoid
+  , simpson
+    ) where
+
+-- | A function, a lower bound, an upper bound and returns the integrated value.
+type Integrator = (Double -> Double) -> Double -> Double -> Double
+
+midpoint :: Int -> Integrator
+midpoint intervals f lBound uBound = dx * sum (map f points)
+    where
+        dx = (uBound - lBound) / fromIntegral intervals
+        points = take intervals $ iterate (+dx) (lBound+dx/2)
+
+trapezoid :: Int -> Integrator
+trapezoid intervals f lBound uBound = dx * ((f lBound + f uBound) / 2 + sum (map f points))
+    where
+        dx = (uBound - lBound) / fromIntegral intervals
+        points = take (intervals-1) $ iterate (+dx) (lBound+dx)
+
+simpson :: Int -> Integrator
+simpson intervals f l u =( 2 * midpoint intervals f l u  + trapezoid intervals f l u ) / 3
