4f54655288430d5b4bd17575e682f5bd88e7766f
Timothy Dees <timothy.dees@gmail.com>
2015-03-20 14:47:18 -0700

    Cleaned up file

--- src/Control/Monad/MonteCarlo.hs
+++ src/Control/Monad/MonteCarlo.hs
@@ -5,33 +5,16 @@
 module Control.Monad.MonteCarlo (
 	MonteCarlo
   ,	MonteCarloT
-  , runMC
-  , calc 
-  , test)
+  , runMC )
 where
 
 import Data.Random
 import Control.Monad.State
 import Data.Functor.Identity
-import System.Random.MWC
 
-type MonteCarloT m a b = StateT a (RVarT m) b
+type MonteCarloT m s a = StateT s (RVarT m) a
 
-type MonteCarlo a b = MonteCarloT Identity a b
-
-calc :: (RandomSource m s) => s -> Double -> m Double
-calc rnd initSt = runMC f rnd initSt 
-	where f = do
-		x <- get
-		y <- lift stdNormalT
-		z <- lift stdNormalT
-		put $ x+y+z
-
+type MonteCarlo s a = MonteCarloT Identity s a
 
 runMC :: RandomSource m s => MonteCarloT m a b -> s -> a -> m a
 runMC mc randState initState = runRVarT (execStateT mc initState) randState
-
-test :: IO Double
-test = do
-	x <- create
-	calc x 0.0
