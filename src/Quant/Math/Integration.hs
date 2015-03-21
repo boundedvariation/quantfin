1b8f9523a624ac5fcb8d39291c532076e9a8e971
Timothy Dees <timothy.dees@gmail.com>
2015-03-20 17:04:20 -0700

    Added comments

--- src/Quant/Math/Integration.hs
+++ src/Quant/Math/Integration.hs
@@ -8,17 +8,20 @@ module Quant.Math.Integration (
 -- | A function, a lower bound, an upper bound and returns the integrated value.
 type Integrator = (Double -> Double) -> Double -> Double -> Double
 
+-- | Midpoint integration.
 midpoint :: Int -> Integrator
 midpoint intervals f lBound uBound = dx * sum (map f points)
     where
         dx = (uBound - lBound) / fromIntegral intervals
         points = take intervals $ iterate (+dx) (lBound+dx/2)
 
+-- | Trapezoidal integration.
 trapezoid :: Int -> Integrator
 trapezoid intervals f lBound uBound = dx * ((f lBound + f uBound) / 2 + sum (map f points))
     where
         dx = (uBound - lBound) / fromIntegral intervals
         points = take (intervals-1) $ iterate (+dx) (lBound+dx)
 
+-- | Integration using Simpson's rule.
 simpson :: Int -> Integrator
 simpson intervals f l u =( 2 * midpoint intervals f l u  + trapezoid intervals f l u ) / 3
