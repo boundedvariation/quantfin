5232c42a3c01c4afba33d9353827d167b8ab0c82
Timothy Dees <timothy.dees@gmail.com>
2015-03-25 16:13:05 -0700

    Linting...

--- src/Quant/YieldCurve.hs
+++ src/Quant/YieldCurve.hs
@@ -1,5 +1,5 @@
 module Quant.YieldCurve (
-	YieldCurve (..)
+    YieldCurve (..)
  ,  FlatCurve (..)
 ) where
 
@@ -10,20 +10,20 @@ basic operations of a yield curve.
 Minimal complete definition: 'disc'.
 -}
 class YieldCurve a where
-	-- | Calculate the discount factor for a given maturity.
-	disc :: YieldCurve a => a -> Double -> Double
+    -- | Calculate the discount factor for a given maturity.
+    disc :: YieldCurve a => a -> Double -> Double
 
-	-- | Calculate the forward rate between a t1 and t2
-	forward :: YieldCurve a => a -> Double -> Double -> Double
-	forward yc t1 t2 = (/(t2-t1)) $ log $ disc yc t1 / disc yc t2
+    -- | Calculate the forward rate between a t1 and t2
+    forward :: YieldCurve a => a -> Double -> Double -> Double
+    forward yc t1 t2 = (/(t2-t1)) $ log $ disc yc t1 / disc yc t2
 
-	-- | Calculate the spot rate for a given maturity.
-	spot :: YieldCurve a => a -> Double -> Double
-	spot yc t = forward yc 0 t
+    -- | Calculate the spot rate for a given maturity.
+    spot :: YieldCurve a => a -> Double -> Double
+    spot yc t = forward yc 0 t
 
 -- |A flat curve is just a flat curve with one continuously 
 -- compounded rate at all points on the curve.
 data FlatCurve = FlatCurve Double
 
 instance YieldCurve FlatCurve where
-	disc (FlatCurve r) t = exp $ -r*t
+    disc (FlatCurve r) t = exp (-r*t)
