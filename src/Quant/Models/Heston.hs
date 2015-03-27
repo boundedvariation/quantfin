df514ae20ba4858813291d47af91b8b63d07296a
Timothy Dees <timothy.dees@gmail.com>
2015-03-27 10:07:23 -0700

    Cleaned up a few bugs

--- src/Quant/Models/Heston.hs
+++ src/Quant/Models/Heston.hs
@@ -36,14 +36,14 @@ instance Discretize Heston where
         fwd <- forwardGen h t2
         let grwth = U.map (\(g, v) -> (g - v/2) * (t2-t1)) (U.zip fwd vState)
             t = t2-t1
-        states <- U.forM (U.zip3 grwth sState vState) $ \ ( g , x, v ) -> do
+        states <- U.forM (U.zip3 grwth sState vState) $ \ ( g, x, v ) -> do
              resid1  <- lift stdNormal
              resid2' <- lift stdNormal
              let 
                 op = if anti then (-) else (+)
                 resid2 = rho * resid1 + sqrt (1-rho*rho) * resid2'
-                v' = (sqrt v `op` eta/2.0*sqrt t* resid2)^(2 :: Int)-l*(v-vf)*t-eta*eta*t/4.0
-                s' = x * exp (g `op` resid1*sqrt v)
+                v' = (sqrt v `op` (eta/2.0*sqrt t* resid2))^(2 :: Int)-l*(v-vf)*t-eta*eta*t/4.0
+                s' = x * exp (g `op` (resid1*sqrt (v*(t2-t1))))
              return (s', abs v')
         let newS = U.map fst states
             newV = U.map snd states
