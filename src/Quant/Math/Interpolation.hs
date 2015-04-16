965d705a1b74caaf4b83df2c6e0aaee7b59ba13e
Timothy Dees <timothy.dees@gmail.com>
2015-04-16 14:55:26 -0700

    Added two new interpolators

--- src/Quant/Math/Interpolation.hs
+++ src/Quant/Math/Interpolation.hs
@@ -1,12 +1,15 @@
 module Quant.Math.Interpolation (
     linearInterpolator
+  , linearVarianceInterpolator
+  , cSplineInterpolator
   , Interpolator1d
-  , Interpolator2d
   --, cubicSpline
 ) where
 
+import Quant.Math.Utilities (tdmaSolver)
+import Data.List (zipWith5)
+
 type Interpolator1d = [Double] -> [Double] -> Double -> Double
-type Interpolator2d = [Double] -> [Double] -> [[Double]] -> Double -> Double -> Double
 
 linearInterpolator :: Interpolator1d
 linearInterpolator (x1:x2:xs) (y1:y2:ys) x
@@ -16,3 +19,33 @@ linearInterpolator (x1:x2:xs) (y1:y2:ys) x
         where
           wt1 = (x2-x) / (x2-x1)
           wt2 = (x-x1) / (x2-x1)
+linearInterpolator _ [y] _ = y
+
+linearVarianceInterpolator :: Interpolator1d
+linearVarianceInterpolator xs ys = linearInterpolator xs 
+                                 . map (\(x, y) -> y*y*x) 
+                                 $ zip xs ys
+
+cSplineInterpolator :: Interpolator1d
+cSplineInterpolator xs ys x = evalSpline xs ys moments
+  where
+    h = zipWith (-) (tail xs) (init xs)
+    lambda = 0.0 : zipWith (\a b->a/(a+b)) (tail h) (init h)
+    mu = map (\a->1.0-a) lambda++[0.0]
+    dj hj hj1 yj1 yj yjm1= 6.0/(hj+hj1)*((yj1-yj)/hj1-(yj-yjm1)/hj)
+    d = 0.0 : zipWith5 dj (init h) (tail h) 
+        (tail $ tail ys) (tail $ init ys) 
+        (init $ init ys) ++ [0.0] 
+    moments = tdmaSolver mu (replicate (length d) 2) lambda d
+    evalSpline (x1:x2:xs') (y1:y2:ys') (m1:m2:ms)
+      | x <= x1   = y1
+      | x >= x2   = evalSpline (x2:xs') (y2:ys') (m2:ms)
+      | otherwise = y1+beta*term+gamma*term*term+
+                      delta*term*term*term
+          where
+            gamma = m1/2.0
+            beta = (y2-y1)/h'-(2*m1+m2)*h'/6.0
+            delta = (m2-m1)/6.0/h'
+            term = x-x1
+            h' = x2-x1
+    evalSpline _ (y':_) _ = y'
