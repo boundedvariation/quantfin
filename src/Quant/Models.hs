6004538ad0e2e58aee994665da7c383ea2370492
Timothy Dees <timothy.dees@gmail.com>
2015-05-10 22:47:17 -0400

    Took Models out of the build for now

--- src/Quant/Models.hs
+++ src/Quant/Models.hs
@@ -3,7 +3,8 @@ module Quant.Models (
 ) where
 
 import Data.Complex
-import Quant.YieldCurve
+import Quant.Math.Integration
+import Quant.ContingentClaim(OptionType(..))
 
 
 {- | The 'CharFunc' class defines those
@@ -15,29 +16,27 @@ Minimal complete definition: 'charFunc'.
 Still under construction.
 -}
 class CharFunc a where
-    -- | Creates a characteristic function for a model, without martingale adjustment.
+    -- | Creates a characteristic function for a model.
     charFunc :: CharFunc a => a -> Double -> Complex Double -> Complex Double
 
-    -- | Calculates characteristic function given a forward generator and yield curve.
-    charFuncMart :: (CharFunc a, YieldCurve b) => a -> b -> Double -> Complex Double -> Complex Double
-    charFuncMart model fg t k = exp (i * r * k) * baseCF k
-      where 
-        i = 0 :+ 1
-        baseCF = charFunc model t
-        r = forward fg 0 t :+ 0
 
-    charFuncOption :: (CharFunc a, YieldCurve b, YieldCurve c) => 
-        a -> b -> c -> ( (Double -> Double) -> Double) -> Double 
+    charFuncOption :: (CharFunc a) => a -> OptionType -> Integrator -> Double 
         -> Double -> Double -> Double
-    charFuncOption model fg yc intF strike tmat damp = intF f
-      where
-        f v' = realPart $ exp (i*v*k) * leftTerm * rightTerm
-          where
-            v = v' :+ 0
-            damp' = damp :+ 0
-            k = log strike :+ 0
-            i = 0 :+ 1
-            leftTerm = d / (damp' + i * v) / (damp'+i*v+(1:+0))
-            rightTerm = cf $ v - i * (damp' + 1)
-            d = disc yc tmat :+ 0
-            cf x = charFuncMart model fg tmat x
+    charFuncOption model opt intF strike tmat damp = case opt of
+        Put  -> s * q * p1 - k * disc * p2
+        Call -> k * disc * (1-p2) - s * q * (1-p1)
+            where
+                s = 
+                p1 = 0.5 + intF func1 1E-8 100 / pi
+                p2 = 0.5 + intF func2 1E-8 100 / pi
+
+                f v' = realPart $ exp (i*v*k) * leftTerm * rightTerm
+                    where
+                        v = v' :+ 0
+                        damp' = damp :+ 0
+                        k = log strike :+ 0
+                        i = 0 :+ 1
+                        leftTerm = d / (damp' + i * v) / (damp'+i*v+(1:+0))
+                        rightTerm = cf $ v - i * (damp' + 1)
+                        d = disc yc tmat :+ 0
+                        cf x = charFuncMart model fg tmat x
