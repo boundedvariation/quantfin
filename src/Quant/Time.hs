78403ca86e84a8473cfe5ec589274f637346d6fb
Timothy Dees <timothy.dees@gmail.com>
2015-05-19 22:54:51 -0400

    Add INLINE pragmas

--- src/Quant/Time.hs
+++ src/Quant/Time.hs
@@ -10,9 +10,12 @@ data Time = Time Double deriving (Eq,Show,Ord)
 
 timeDiff :: Time -> Time -> Double
 timeDiff (Time x) (Time y) = y - x
+{-# INLINE timeDiff #-}
 
 timeOffset :: Time -> Double -> Time
 timeOffset (Time x) y = Time (x+y)
+{-# INLINE timeOffset #-}
 
 timeFromZero :: Time -> Double
 timeFromZero (Time x) = x
+{-# INLINE timeFromZero #-}
--- src/Quant/YieldCurve.hs
+++ src/Quant/YieldCurve.hs
@@ -18,10 +18,12 @@ class YieldCurve a where
     -- | Calculate the forward rate between a t1 and t2
     forward :: YieldCurve a => a -> Time -> Time -> Double
     forward yc t1 t2 = (/(timeFromZero t2-timeFromZero t1)) $ log $ disc yc t1 / disc yc t2
+    {-# INLINE forward #-}
 
     -- | Calculate the spot rate for a given maturity.
     spot :: YieldCurve a => a -> Time -> Double
     spot yc t = forward yc (Time 0) t
+    {-# INLINE spot #-}
 
 -- |A flat curve is just a flat curve with one continuously 
 -- compounded rate at all points on the curve.
